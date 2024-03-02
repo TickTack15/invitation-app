318b0f5946ce464a232f6e25cd829d2bbdd28fdd
Vladyslav Honsales <romalesbox@gmail.com>
2024-03-02 23:31:00 +0100

    removed asssets folder

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Success = ({count}) => {
   return (
     <div className="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+      <img src="../assets/success.svg" alt="Success" />
       <h3>Success!</h3>
       <p>An invitation has been sent to all {count} users.</p>
       <button onClick={() => window.location.reload()} className="send-invite-btn">
