b8dd84f80a7b189316e7766a2c958ec5bb140e85
Vladyslav Honsales <romalesbox@gmail.com>
2024-03-02 02:21:22 +0100

    dynamic list + search filter + skeleton without animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Users } from "./components/Users";
 
 // Users list: https://reqres.in/api/users
 
-function App() { 
+function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(json => {
+        setUsers(json.data);
+      })
+      .catch(err => {
+        console.warn(err);
+        alert('Error when receiving users')
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,23 +1,43 @@
 import React from "react";
+import { Skeleton } from "./Skeleton";
 import { User } from "./User";
 
-export const Users = () => {
+export const Users = ({ items, isLoading, searchValue, onChangeSearchValue }) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Find a user..."/>
+        <input
+          value={searchValue}
+          onChange={onChangeSearchValue}
+          type="text"
+          placeholder="Find a user..."
+        />
       </div>
-      <div className="users-list">
-        <ul>
-          <User />
+      {isLoading ? (
+        <div className="skeleton-list">
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </div>
+      ) : (
+        <ul className="users-list">
+            {
+              items.filter(user => {
+                const fullName = (user.first_name + user.last_name).toLowerCase();
+                
+                return fullName.includes(searchValue.toLowerCase()) || user.email.toLowerCase().includes(searchValue.toLowerCase());
+              }).map((item) => (
+                <User {...item} />
+              ))
+            }
         </ul>
-        <button className="send-invite-button">
-          Send an invitation
-        </button>
-      </div>
+      )}
+      <button className="send-invite-button">
+        Send an invitation
+      </button>
     </>
   );
 }
